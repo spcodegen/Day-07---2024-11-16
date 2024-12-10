fccaed1890defb0048b99e041902446b1f00b5ea
spcodegen <shehansp2000@gmail.com>
2024-12-10 10:54:33 +0530

    get username after clicked add button call addName method

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const names=["saman","kamal","nimal","sunil","shehan"];
-document.getElementById("title").innerHTML = names.toString();
-document.write(names);
 
-//document.getElementById("ordertList").innerHTML = "<li>name[0]</li><li>name[1]</li><li>name[2]</li><li>name[3]</li>";
-let body="";
-for (const name of names) {
-    body+=`<li>${name}</li>`;
+loadData();
+
+function loadData(){
+    let body="";
+    for (const name of names) {
+        body+=`<li>${name}</li>`;
+    }
+    document.getElementById("ordertList").innerHTML = body;
 }
-document.getElementById("ordertList").innerHTML = body;
+
+function addName(){
+    let inputName = document.getElementById("txtName").value;
+    names.push(inputName);
+    loadData();
+}
+
+
+
+
+
 
